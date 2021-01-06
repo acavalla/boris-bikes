5b944ebf6c4a9f33cfaf3dd167baed39fe709620
Hugh Cavanagh <hacavanagh@gmail.com>
2021-01-06 15:04:26 +0000

    Wrote test in docking_station_spec.rb that the docking station releases a bike that's working and got this test to pass

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,8 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
+  it 'Should release a working bike' do
+    bike = DockingStation.new.release_bike
+    expect(bike.working?).to eq true
+  end
 end
