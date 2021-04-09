292f110a1b81b7a6f8c89e07d13c6cbcbb3ab331
Anna Cavalla <Anna.Cavalla@gmail.com>
2021-04-09 16:19:09 +0100

    Refactored van spec

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -1,21 +1,25 @@
 require 'van'
 
 describe Van do
-  describe '.accept_bikes' do
-    let(:bike) {double 'bike'}
-    let(:bike2) {double 'bike'}
+  let(:bike) {double 'bike'}
+  let(:bike2) {double 'bike'}
+  describe '.initialize' do
     it 'initializes with an empty array of bikes' do
       expect(subject.bikes).to be_empty
     end
-
-    it 'accepts bikes' do
+  end
+  describe '.accept_bikes' do
+    it 'stores bikes in bikes array' do
       subject.accept_bikes([bike, bike2])
       expect(subject.bikes).to include bike, bike2
     end
+  end
 
-    it 'releases bikes' do
+  describe '.release_bikes' do
+    it 'removes bikes from bikes array' do
       subject.release_bikes([bike, bike2])
       expect(subject.bikes).not_to include bike, bike2
     end
   end
+
 end
