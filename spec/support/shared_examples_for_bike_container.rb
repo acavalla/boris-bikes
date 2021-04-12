e385e85af8f846ed9d02cfe9a82692e818aee882
Anna Cavalla <Anna.Cavalla@gmail.com>
2021-04-12 16:13:18 +0100

    Added removing bike methods to bike container

--- spec/support/shared_examples_for_bike_container.rb
+++ spec/support/shared_examples_for_bike_container.rb
@@ -25,4 +25,23 @@ shared_examples_for BikeContainer do
     end
   end
 
+  describe 'remove bike' do
+    let(:bike) { double 'bike' }
+    before(:each) { subject.add_bike bike }
+
+    it 'returns a bike' do
+      expect(subject.remove_bike).to eq bike
+    end
+
+    it 'removes the bike from the collection' do
+      subject.remove_bike
+      expect(subject).to be_empty
+    end
+
+    it 'raises an error when empty' do
+      subject.remove_bike
+      expect { subject.remove_bike }.to raise_error "#{described_class.name} empty"
+    end
+  end
+
 end
