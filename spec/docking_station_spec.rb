585c97d5729f816b433e303eed3bf83cb92003a7
Anna Cavalla <Anna.Cavalla@gmail.com>
2021-04-14 13:10:22 +0100

    Refactored docking station spec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,22 +6,27 @@ require 'docking_station'
 describe DockingStation do
   let(:bike) { double :bike, working?: true }
   let(:bike2) { double :bike, working?: false }
-  describe '.release_bike' do
-    it 'should not release a broken bike' do
+
+  describe "methods not covered by BikeContainer" do
+    before do
       subject.dock(bike)
       subject.dock(bike2)
-      expect(subject.release_bike).to eq bike
+    end
+
+    describe '.release_bike' do
+      it 'should not release a broken bike' do
+        expect(subject.release_bike).to eq bike
+      end
+    end
+
+    describe '.pickup' do
+      it 'removes all the broken bikes' do
+        subject.pickup
+        expect(subject.bikes).to eq [bike]
+      end
     end
   end
 
-  describe '.pickup' do
-    it 'removes all the broken bikes' do
-      subject.dock(bike)
-      subject.dock(bike2)
-      subject.pickup
-      expect(subject.bikes).to eq [bike]
-    end
-  end
 
   it_behaves_like BikeContainer
   # it { is_expected.to have_attributes(assigns(:bike) == 0) }
